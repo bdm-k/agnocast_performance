27072f993bd03700a3d4f7e8691183638b4db383
bdm-k <kokusyunn@gmail.com>
2025-06-10 14:49:04 +0900

    Support the second composition pattern

--- src/agnocast_sample_application/launch/listener.launch.py
+++ src/agnocast_sample_application/launch/listener.launch.py
@@ -44,7 +44,23 @@ def generate_launch_description():
             ),
         ])
     elif composition_pattern == 1:
-        raise Exception('Unsupported composition pattern')
+        return LaunchDescription([
+            Node(
+                package="agnocast_sample_application",
+                executable="listener",
+                output="screen",
+                parameters=[{
+                    'starting_topic_id': topic_id,
+                    'num_topics': 1,
+                    'use_multithreaded_executor': use_multithreaded_executor,
+                }],
+                additional_env={
+                    'LD_PRELOAD': prepend_libagnocast_heaphook(os.environ.get('LD_PRELOAD', '')),
+                    'AGNOCAST_MEMPOOL_SIZE': '16777216',  # 16 MB
+                },
+            )
+            for topic_id in range(0, num_topics)
+        ])
     elif composition_pattern == 2:
         raise Exception('Unsupported composition pattern')
     else:
